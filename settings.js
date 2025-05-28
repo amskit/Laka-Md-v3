e6785bec2186b5c1b5242d70b3fc3ab85e38354e
ameermuawiya <81509990+ameermuawiya@users.noreply.github.com>
2025-05-28 21:48:20 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=04R1RYCJ#KzZ754W9oN3kjSXHBW0UghVye5mBM8eRQj1tHiRxaz0",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
